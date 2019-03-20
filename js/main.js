289b774dc0c5846480fce22c9de6e03549d12c75
Nathan DECHER <nathan.decher@gmail.com>
2019-03-20 10:48:10 +0100

    content can now be hidden

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 	else document.addEventListener('DOMContentLoaded', fn);
 })(() => {
 	document.querySelector('#mainCard .readmore').addEventListener('click', () => {
-		document.querySelector('#contentBox').classList.add('active');
+		document.querySelector('#contentWrapper').classList.add('active');
+	});
+	document.querySelector('#contentWrapper .exit').addEventListener('click', () => {
+		document.querySelector('#contentWrapper').classList.remove('active');
 	});
 });
